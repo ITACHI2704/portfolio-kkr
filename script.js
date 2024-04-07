cb836358675d9169d65d9478c8e718da3486252a
krishan kumar <krishankumar39848@gmail.com>
2024-04-07 17:40:25 +0530

    changes

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+
+// contact form ///
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxrZsjAEtQH_-OheTkdNsbnWc3DuYBKelzjTQO0eRBRBn7U5S9HUZPgTbpdUB_hoZpCMg/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response =>{
+      msg.innerHTML="Message sent sucessfully"
+      setTimeout(function (){
+        msg.innerHTML=""
+      },2000)
+      form.reset()
+    })
+    .catch(error => console.error('Error!', error.message))
+})
+
+
+
 let  menu = document.querySelector(" i.bx-menu-alt-right");
 let cross = document.querySelector(" i.bx-x");
 let navbar = document.querySelector(".hlinks");
@@ -12,17 +34,10 @@ navbar.style.transform = "translateX(0px)";
 })
 
 
-
 cross.addEventListener("click", ()=>{
     navbar.style.transform = "translateX(800px)";
   
     })
-
-
-
-
-
-
 
 
       //////  portfolio form /////////////////
@@ -36,18 +51,6 @@ poster.addEventListener("mouseover",()=>(
 ))
 
 
-    //////////////////////////////////contact form 
 
-
-
-  const scriptURL = 'https://script.google.com/macros/s/AKfycbwwsYU3VzDbjdwtXqyCspldNzjJRDwUYQC_7A2hm6mHxYQz3RMk-CoONW-GJBkfkXrW/exec'
-  const form = document.forms['submit-to-google-sheet']
-
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!', response))
-      .catch(error => console.error('Error!', error.message))
-  })
 
 
